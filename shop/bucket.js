df9c052d6d9b3e9d9587adc8a0ac3a60b35090e8
Vladimir <mareev.vladimir95@gmail.com>
2021-06-22 00:30:14 +0300

    Shop
    Catalog and busket in bucket.html, bucket.js files

--- shop/bucket.js
+++ shop/bucket.js
@@ -1,82 +1,183 @@
 const USD = 'usd';
 const RUB = 'rub';
 
-let busket = {
-	goods: [
-		{
-			name: "IPhone12",
-			price: 1000,
-			currency: USD,
-			count: 1,
-			discount: 15
-		},
-		{
-			name: "Macbook Air 13 M1",
-			price: 85000,
-			currency: RUB,
-			count: 1,
-			discount: 5
-		}
-	],	
-
-	countBasketPrice(currency) {
-		let totalPrice = 0;
-		this.goods.forEach(function(good, index, array) {			
-			totalPrice += countPrice(good, (good.currency == USD ? currency : 1));			
-		});
-		return totalPrice.toFixed(2);
+function convertCurrency(curr) {
+	switch(curr) {
+		case USD:
+			return 71;
+		case RUB:
+		default:
+			return 1;
 	}
 }
 
-function countPrice(good, currency) {
-	return currency * good.price * (1 - good.discount/100) * good.count;		;
+let product = {
+	goods: [],	
+
+	add(name, price, currency, discount) {
+		let good = this.goods.find(good => good.name == name);
+		if (good != null) {
+			good.count += 1;
+		}
+		else {
+			this.goods.push({
+				name: name,
+				price: price,
+				discount: discount,
+				currency: currency,	
+			});
+		}			
+	},	
+}
+
+let busket = {
+	goods: [],	
+
+	addGood(name) {
+		let good = this.goods.find(good => good.name == name);
+		if (good != null) {
+			good.count += 1;
+		}
+		else {
+			this.goods.push({
+				name: name,				
+				count: 1,
+			});
+		}			
+	},
+
+	countBasketPrice() {
+		let totalPrice = 0;		
+		for (let i = 0; i < this.goods.length; ++i) {
+			let busketItem = this.goods[i];
+			let productItem = product.goods.find( (good) => {
+				return good.name === busketItem.name;
+			});
+			totalPrice += countPrice(productItem, busketItem.count);
+		}
+		return totalPrice.toFixed(2);
+	},
+
+	goodsCount() {
+		let count = 0;
+		for (let i = 0; i < this.goods.length; ++i) {
+			count += this.goods[i].count;
+		}
+		return count;
+	}
+};
+
+function createBusketID(name) {
+	return 'busket-item-' + name;
+}
+
+
+let form = {
+	buildBusket() {	
+		let busketNode = document.querySelector('.busket');
+		busketNode.innerHTML = '';
+		
+		for (let i = 0; i < busket.goods.length; ++i) {
+			let busketItem = busket.goods[i];
+			let productItem = product.goods.find( (good) => {
+				return good.name === busketItem.name;
+			} );
+			
+			if (document.getElementById(createBusketID(busketItem.name)) != null)
+				continue;
+
+			let item = document.createElement('div');
+			
+			item.id = createBusketID(busketItem.name);
+			item.className = 'busket-item'
+			busketNode.appendChild(item);
+
+			let itemName = document.createElement('p');
+			itemName.innerText = busketItem.name;
+
+			let itemCount = document.createElement('div');
+			itemCount.className = 'busket-item-count';
+			itemCount.innerText = busketItem.count;
+
+
+			let itemPrice = document.createElement('p');
+			itemPrice.innerText = productItem.price;
+			itemPrice.className = 'busket-item-price';
+
+
+			item.appendChild(itemName);
+			item.appendChild(itemPrice);
+			item.appendChild(itemCount);
+		}
+		
+		let itemSum = document.querySelector('.busket-sum');
+		
+		if (busket.goodsCount() == 0) 
+			itemSum.innerText = 'Корзина пуста'
+		else 
+			itemSum.innerText = 'Товаров ' + busket.goodsCount() + ' на сумму ' + busket.countBasketPrice();
+	},
+
+	buildProduct() {
+		let productNode = document.querySelector('.product');
+		productNode.innerHTML = '';
+		
+		for (let i = 0; i < product.goods.length; ++i) {
+			let productItem = product.goods[i];
+			
+			let item = document.createElement('div');
+						
+			item.className = 'product-item';
+			productNode.appendChild(item);
+
+			let itemName = document.createElement('p');
+			itemName.className = 'catalog-list-item-name';
+			itemName.innerText = productItem.name;
+
+			let itemPrice = document.createElement('p');
+			itemPrice.className = 'catalog-item-price';
+			itemPrice.innerText = productItem.price + ' ' + productItem.currency;			
+			
+			let itemImage = document.createElement('img');
+			itemImage.className = 'catalog-item-image';
+			itemImage.src = './img/' + productItem.name.replaceAll(' ', '-').toLowerCase() + '-min.jpg';			
+			itemImage.alt = productItem.name;
+
+			let itemButton = document.createElement('input');
+			itemButton.className="button-buy";
+			itemButton.type="button";
+			itemButton.value="В корзину";	
+			itemButton.addEventListener('click', onButtonBuyClick);
+
+			item.appendChild(itemName);
+			item.appendChild(itemPrice);
+			item.appendChild(itemImage);
+			item.appendChild(itemButton);
+
+		}
+	}
 };
 
 
-/*************************
- * Currency converter
- * */
-let converter = {			
-	httpGet(fromCurrency, toCurrency)
-	{		    	
-		const theUrl = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${fromCurrency}/${toCurrency}.json`		
-		return new Promise((resolve, reject) => {
-	    	const https = require('https');
-			
-			https.get(theUrl, (response) => {	    
-			    let jsonText = '';		  		
-				response.on('data', (chunk) => {
-				    jsonText += chunk;
-				});		  		
-				response.on('end', () => {
-				  	const jsonRes = JSON.parse(jsonText); 		  	
-				  	resolve(jsonRes[toCurrency])
-				});
+function countPrice(good, count) {
+	return convertCurrency(good.currency) * good.price * (1 - good.discount/100) * count;
+};
 
-			}).on("error", (error) => {
-			    reject("Error currency request: " + error.message);
-			});
-		
-		});
-	}
+
+function onButtonBuyClick(event) {
+	let item = this.parentNode;
+	let name = item.querySelector('.catalog-list-item-name').innerText;	
+	busket.addGood(name);
+	form.buildBusket();
 }
 
-function handleError(errMsg) {
-	console.log(errMsg);
-}
 
-function handleSuccess(currency) {
-	busketPrice = busket.countBasketPrice(currency);
-	console.log(busketPrice, RUB)
-}
 
-function onClickAddToBusket() {	
-	alert('busket', busketPrice)
+function Gen() {
+	product.add('Macbook Air 13', 85000, RUB, 0);
+	product.add('IPhone 12', 1000, USD, 15);
+	product.add('Airpods', 20000, RUB, 20);	
+	console.log(busket.goods.length);
+	form.buildProduct();
+	form.buildBusket();
 }
-
-function Main() {
-	let resolve = converter.httpGet('usd', 'rub');
-	resolve.then(handleSuccess, handleError);
-}
-
-Main();
